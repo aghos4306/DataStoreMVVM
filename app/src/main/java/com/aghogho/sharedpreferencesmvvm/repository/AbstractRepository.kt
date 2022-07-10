5c5d7396dd67d7a8af458d20f5bedf2ddccca603
Aghogho <aghogho.akpedeye@gmail.com>
2022-07-10 13:22:50 +0100

    created databinding for xml and its fields, create datastore fun to write and read data from datastore

--- app/src/main/java/com/aghogho/sharedpreferencesmvvm/repository/AbstractRepository.kt
+++ app/src/main/java/com/aghogho/sharedpreferencesmvvm/repository/AbstractRepository.kt
@@ -1,9 +1,12 @@
 package com.aghogho.sharedpreferencesmvvm.repository
 
+import com.aghogho.sharedpreferencesmvvm.Phonebook
+import kotlinx.coroutines.flow.Flow
+
 interface AbstractRepository {
 
-    suspend fun savePhoneBook()
+    suspend fun savePhoneBook(phonebook: Phonebook)
 
-    suspend fun getPhoneBook()
+    suspend fun getPhoneBook(): Flow<Phonebook>
 
 }
--- app/src/main/java/com/aghogho/sharedpreferencesmvvm/repository/ImplRepository.kt
+++ app/src/main/java/com/aghogho/sharedpreferencesmvvm/repository/ImplRepository.kt
@@ -1,15 +1,40 @@
 package com.aghogho.sharedpreferencesmvvm.repository
 
 import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import com.aghogho.sharedpreferencesmvvm.Phonebook
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
+const val DataStore_NAME = "PHONEBOOK"
+val Context.datastore: DataStore<Preferences> by preferencesDataStore(name = DataStore_NAME)
 class ImplRepository(
     private val context: Context
 ): AbstractRepository {
-    override suspend fun savePhoneBook() {
-        TODO("Not yet implemented")
+
+    companion object {
+        val NAME = stringPreferencesKey("NAME")
+        val PHONE_NUMBER = stringPreferencesKey("PHONE")
+        val ADDRESS = stringPreferencesKey("ADDRESS")
     }
 
-    override suspend fun getPhoneBook() {
-        TODO("Not yet implemented")
+    override suspend fun savePhoneBook(phonebook: Phonebook) {
+        context.datastore.edit { phonebooks ->
+            phonebooks[NAME] = phonebook.name
+            phonebooks[PHONE_NUMBER] = phonebook.phone
+            phonebooks[ADDRESS] = phonebook.address
+        }
+    }
+
+    override suspend fun getPhoneBook(): Flow<Phonebook> = context.datastore.data.map { phonebook ->
+        Phonebook(
+            name = phonebook[NAME]?: "",
+            address = phonebook[ADDRESS]?: "",
+            phone = phonebook[PHONE_NUMBER]?: ""
+        )
     }
 }
